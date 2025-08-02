6e7957c1b6858d1f1d862dbfbdd86d2cdb967164
tiwariparth <tiwariparth067@gmail.com>
2025-08-02 18:44:34 +0530

    Type Converstion and Type coercion

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,13 +1,25 @@
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
 
-const BMIMark = massMark / (heightMark * heightMark);
-const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
 
-if(BMIMark > BMIJohn){
-     console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})`);
-} else{
-     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})`);
-}
+// if(BMIMark > BMIJohn){
+//      console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})`);
+// } else{
+//      console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})`);
+// }
+
+
+
+// Type Conversion
+// const inputYear = "1000"
+// console.log(Number(inputYear)+12,inputYear);
+
+// Type Coercion
+// console.log(`i am ${23} years old`);
+// console.log('I am '+ 23 + ' years old');
+
+ 
