7573a2ae4b4a2f7af2c732b3e3a9c2889ddd6c02
GeekyMe <tiwariparth@gmail.com>
2025-08-20 12:20:59 +0530

    Implement game logic for number guessing challenge

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1
+let message = document.querySelector('.message')
+let score = 20
+document.querySelector('.number').textContent = secretNumber
+
+document.querySelector('.check').addEventListener('click', function () {
+     const guess = document.querySelector('.guess').value;
+     console.log(guess);
+
+     if (!guess) {
+          message.textContent = 'No number'
+     } else if (guess == secretNumber) {
+          message.textContent = 'Correct Number'
+     } else if (guess > secretNumber) {
+          if (score > 0) {
+               message.textContent = 'Too High'
+               score--
+               document.querySelector('.score').textContent = score
+          } else {
+               message.textContent = 'You lost!'
+          }
+     } else {
+          if (score > 0) {
+               message.textContent = 'Too High'
+               score--
+               document.querySelector('.score').textContent = score
+          } else {
+               message.textContent = 'You lost!'
+          }
+     }
+})
