b995046130e2c250b07b663d36c905a45716728c
GeekyMe <tiwariparth@gmail.com>
2025-08-21 23:02:05 +0530

    Refactor guess checking logic and improve game state management

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,18 +1,25 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1
+let secretNumber = Math.trunc(Math.random() * 20) + 1
 let message = document.querySelector('.message')
 let score = 20
-document.querySelector('.number').textContent = secretNumber
+let highscore = 0
 
-document.querySelector('.check').addEventListener('click', function () {
-     const guess = document.querySelector('.guess').value;
+function checkGuess() {
+     const guess = Number(document.querySelector('.guess').value);
      console.log(guess);
 
      if (!guess) {
           message.textContent = 'No number'
-     } else if (guess == secretNumber) {
+     } else if (guess === secretNumber) {
           message.textContent = 'Correct Number'
+          document.querySelector('body').style.backgroundColor = '#60b347'
+          document.querySelector('.number').style.width = '30rem'
+          document.querySelector('.number').textContent = secretNumber
+          if (score > highscore) {
+               highscore = score
+               document.querySelector('.highscore').textContent = highscore
+          }
      } else if (guess > secretNumber) {
           if (score > 0) {
                message.textContent = 'Too High'
@@ -23,11 +30,31 @@ document.querySelector('.check').addEventListener('click', function () {
           }
      } else {
           if (score > 0) {
-               message.textContent = 'Too High'
+               message.textContent = 'Too low'
                score--
                document.querySelector('.score').textContent = score
           } else {
                message.textContent = 'You lost!'
           }
      }
+}
+
+document.querySelector('.check').addEventListener('click', checkGuess)
+
+document.querySelector('.guess').addEventListener('keydown', function (e) {
+     if (e.key === 'Enter') {
+          checkGuess();
+     }
+})
+
+
+document.querySelector('.again').addEventListener('click', function () {
+     score = 20
+     secretNumber = Math.trunc(Math.random() * 20) + 1
+     message = message.textContent = 'Start guessing...'
+     document.querySelector('.score').textContent = score
+     document.querySelector('.number').style.width = '15rem'
+     document.querySelector('body').style.backgroundColor = '#222'
+     document.querySelector('.number').textContent = '?'
+     document.querySelector('.guess').value = ''
 })
